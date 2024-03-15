e482c9b583c22cebe6f858bff88f80d09c06f3d0
Tech-Vexy <eveliaveldrine@outlook.com>
2024-03-16 00:11:37 +0300

    Added python file user_input.py

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you from? ")
+
+message = f"Hello {name}! You are {age} years old and from {location}."
+print(message)
