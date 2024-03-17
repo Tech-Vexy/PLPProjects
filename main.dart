7809331fba5a24e47d36b06f6b7118ebef1971c3
Tech-Vexy <eveliaveldrine@outlook.com>
2024-03-17 13:08:32 +0300

    Created first dart program

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "Veldrine Evelia";
+  String occupation ="Student";
+  String interest = "Artificial Intelligence";
+  int age = 23;
+  double height = 1.62;
+  num weight = 62;
+  print("""
+  My name is $name,am a $occupation and I have an interest in $interest. I am $age years old,$height metres tall and I have $weight kilograms.
+  """);
+}
